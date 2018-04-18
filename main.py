819fcc1edd472543f7448851ce3de0d489fe1378
Teimur Gasanov <teymurgg321@gmail.com>
2018-04-18 15:59:11 +0600

    collibration

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class App:
             is_color_found = is_color_in_range(found_color, color_to_find_range[0], color_to_find_range[1])
             if not is_color_found:
                 print('NOT IN RANGE {}'.format(color_to_find))
-                self.car.go_forward(0.05)               
+                self.car.go_forward(0.1)               
                 continue
             else:
                 print('IN RANGE {}'.format(color_to_find))                
